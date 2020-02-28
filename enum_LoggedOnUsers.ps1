d16eeb3b099d20b412002998906e91ef1bdd142a
MegaphatNetworks <60324301+MegaphatNetworks@users.noreply.github.com>
2020-02-28 08:33:51 -0500

    Update enum_LoggedOnUsers.ps1

--- enum_LoggedOnUsers.ps1
+++ enum_LoggedOnUsers.ps1
@@ -18,7 +18,7 @@
 	But for some reason When trying to assign the $Session value (not even getting to $Result) I get the error:
 		WinRM cannot complete the operation. Verify that the specified computer name is valid, that the computer is accessible
 		over the network, and that a firewall exception for the WinRM service is enabled and allows access from this computer.
-	even when WinRM is configured and running.  So I decided to use SysInternals PSExec, which is a bit slower but it works.
+	even when WinRM is configured and running.  So I decided to use SysInternals PSExec only if quser returned no results due to permissions.
 
 	So here is the output of quser format (after skipping the first 5 from psexec and 1 from quser)	
 	0         1         2         3         4         5         6         7         8
@@ -43,37 +43,52 @@
 	All in all the script is a bit slow because of the psexec but effective.
 
 #>
-#	$ErrorActionPreference="SilentlyContinue"
-#	$ErrorAction="SilentlyContinue"
-	Write-Progress -Activity "Retrieving Domain Computers" -Status "..."
-	$AllComputers = (Get-ADComputer -Filter * -Properties operatingsystem | sort name).Name	
-	$CompArray = $AllComputers.Split("`n")
-	$cnt = 0
-	foreach ($ThisComputer in $CompArray) {
-		$cnt++
-		If (test-connection -computername $ThisComputer -count 1 -quiet){
-			$act = "Retrieving users for " + $ThisComputer
-			$stat = ($cnt).ToString() +  "/" + ($CompArray.Count).ToString()
-			Write-Progress -Activity $act -Status $stat
-			$pse = (.\psexec \\$ThisComputer quser | select -skip 6) | out-string
-			$lines = $pse.Split("`n")
-			$sTemp =$null
-			for ($i=0;$i -le ($lines.count-1);$i++) {
-				if (!($lines[$i].length -lt 82)) {
-					$sTemp = $sTemp + "[pscustomobject]@{UID='" + $lines[$i].subString(1,19).Trim() + "';"
-					$sTemp = $sTemp + "Method='" + $lines[$i].subString(23,7).Trim() + "';"
-					$sTemp = $sTemp + "SID='" + $lines[$i].subString(43,2).Trim() + "';"
-					$sTemp = $sTemp + "State='" + $lines[$i].subString(46,6).Trim() + "';"
-					$sTemp = $sTemp + "Idle='" + $lines[$i].subString(55,8).Trim() +"';"
-					$sTemp = $sTemp + "Date='" + $lines[$i].subString(65,9).Trim() +"';"
-					$sTemp = $sTemp + "Time='" + $lines[$i].subString(75,7).Trim() +"'}"
-				}
+
+function enumUsers ($ThisComputer) {
+	$ThisComputer
+	$pse = (quser /server:$ThisComputer | select -skip 1) | out-string
+	if ($pse.trim().length -eq 0) {
+		write-host "Retrying quser with PSExec"
+		$pse = (.\psexec \\$ThisComputer quser | select -skip 6) | out-string
+	}
+	Start-Sleep 1
+	if ($pse.trim().length -ne 0) {
+		$lines = $pse.Split("`n")
+		$sTemp =$null
+		for ($i=0;$i -le ($lines.count-1);$i++) {
+			if (!($lines[$i].length -lt 80)) {
+				$sTemp = $sTemp + "[pscustomobject]@{UID='" + $lines[$i].subString(1,19).Trim() + "';"
+				$sTemp = $sTemp + "Method='" + $lines[$i].subString(23,7).Trim() + "';"
+				$sTemp = $sTemp + "SID='" + $lines[$i].subString(42,3).Trim() + "';"
+				$sTemp = $sTemp + "State='" + $lines[$i].subString(46,6).Trim() + "';"
+				$sTemp = $sTemp + "Idle='" + $lines[$i].subString(55,8).Trim() +"';"
+				$sTemp = $sTemp + "Date='" + $lines[$i].subString(65,9).Trim() +"';"
+				$sTemp = $sTemp + "Time='" + $lines[$i].subString(75,7).Trim() +"'}"
 			}
-			$oCU =$null
+		}
+		$oCU =$null
+		if ($sTemp.length -ne 0) {
 			$sTemp = $sTemp.Replace("}[","},[")
 			$inx = '$oCU = @(' + $sTemp + ')'
 			Invoke-Expression $inx
-			$ThisComputer
+			
 			$oCU | ft
 		}
+		write-host ""
 	}
+}
+
+Write-Progress -Activity "Retrieving Domain Computers" -Status "..."
+$AllComputers = (Get-ADComputer -Filter * -Properties operatingsystem | sort name).Name	
+$CompArray = $AllComputers.Split("`n")
+$cnt = 0
+foreach ($TComputer in $CompArray) {
+	$cnt++
+	If (test-connection -computername $TComputer -count 1 -quiet){
+		$act = "Retrieving users for " + $TComputer
+		$stat = ($cnt).ToString() +  "/" + ($CompArray.Count).ToString()
+		Write-Progress -Activity $act -Status $stat
+		enumUsers $TComputer
+	}
+}
+	
